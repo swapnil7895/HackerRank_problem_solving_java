f8bbe3dcc31d895db7a0f1b05c448ee6419f7d8d
swapnil7895 <51965205+swapnil7895@users.noreply.github.com>
2021-02-14 01:36:35 +0530

    Hackerrank problem's solution by me.

---
+++ BirdOccurence.java
@@ -0,0 +1,118 @@
+import java.util.*;
+import java.util.Arrays;
+public class BirdOccurence
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+      if(n<6 && n>2*100*100*10){System.exit(0);}
+      
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]>5){System.exit(0);}
+        }
+        int SPC[]=new int[n];
+        int counter=0;
+        int mx=0;
+        int mx_index=0;
+        int mx_no=0;
+        int t=0;
+        Arrays.sort(arr);
+//1123
+        for(int y=0;y<n-1;y++)
+        {
+            counter++;
+            if(arr[y]==arr[y+1])
+            { 
+                counter++;
+                if(counter>mx)
+                {mx_no=arr[y];}
+             }
+            else
+            {
+                counter--;
+                if(mx==counter && arr[y]<arr[t])
+                {     mx_no=arr[y];    }
+                if(mx<counter)
+                {
+                    mx=counter;
+                    mx_no=arr[y];
+                    t=y;
+                }
+                counter=0;
+            }
+        }
+        System.out.println(mx_no);
+    }
+}
+
+  //   int maxx=arr[0];
+   //     int max_index=0;
+        
+        // for(int s=1;s<n;s++)
+        // {
+        //     if(maxx==SPC[s] && arr[max_index]>arr[s])
+        //     {
+        //             max_index=s;
+
+        //     }
+        //     if(SPC[s]>maxx)
+        //     {
+        //         maxx=SPC[s];
+        //         max_index=s;
+        //     }
+        // }    
+
+
+
+/*
+import java.util.*;
+public class BirdOccurence
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+      if(n<6){System.exit(0);}
+      
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int SPC[]=new int[n];
+        int counter=0;
+        for(int j=0;j<n;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if(arr[j]==arr[k])
+                {
+                    counter++;
+                }
+            }   
+            SPC[j]=counter;
+            counter=0;
+            
+        }
+        int maxx=arr[0];
+        int max_index=0;
+        
+        for(int s=1;s<n;s++)
+        {
+            if(maxx==SPC[s] && arr[max_index]>arr[s])
+            {
+                    max_index=s;
+
+            }
+            if(SPC[s]>maxx)
+            {
+                maxx=SPC[s];
+                max_index=s;
+            }
+        }    
+        System.out.println(arr[max_index]);
+    }
+}
+*/
