729e41e144ea1c48a07169bddb17c993517b041f
swapnil7895 <51965205+swapnil7895@users.noreply.github.com>
2021-02-14 01:41:11 +0530

    added problem statement

--- BirdOccurence.java
+++ BirdOccurence.java
@@ -1,3 +1,10 @@
+/*
+Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type.
+If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+PROBLEM STATEMENT LINK --    https://www.hackerrank.com/challenges/migratory-birds/problem
+*/
+
+
 import java.util.*;
 import java.util.Arrays;
 public class BirdOccurence
